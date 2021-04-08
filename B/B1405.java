5f4b176be3e3f3c2a7a39737b98dd76154925d11
Darknez07 <rishikakkar42@gmail.com>
2021-04-09 02:03:45 +0530

    Level B 1400 series

---
+++ B/B1405.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+// Not completed
+public class B1405 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        int n,steps,mid;
+        while(t--!=0){
+            n = input.nextInt();
+            ArrayList<Integer> lst = new ArrayList<Integer>();
+            for(int i=0;i<n;i++){
+                lst.add(input.nextInt());
+            }
+        }
+        input.close();
+    }
+}
