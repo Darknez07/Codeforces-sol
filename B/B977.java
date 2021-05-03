d99e9f4243e820f372f2723f43ef4806eda8f196
Darknez07 <rishikakkar42@gmail.com>
2021-05-04 01:12:50 +0530

    New Level B

---
+++ B/B977.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class B977 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int l = input.nextInt();
+        HashSet<String> st = new HashSet<String>();
+        ArrayList<String> lst = new ArrayList<String>();
+        String s = input.next();
+        String p="";
+        input.close();
+        for(int i=0;i<s.length() - 1;i++){
+            p = s.substring(i,i+2);
+            st.add(p);
+            lst.add(p);
+        }
+        int max = -1;
+        int occur;
+        for(String sn: st){
+            occur = Collections.frequency(lst, sn);
+            if(occur > max){
+                max = occur;
+                p = sn;
+            }
+        }
+        System.out.println(p);
+    }
+}
