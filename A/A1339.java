151c8ac1cbd949f18ab7138cbebd6920d2137fb2
Darknez07 <rishikakkar42@gmail.com>
2021-06-20 01:54:03 +0530

    Level A series 1300

---
+++ A/A1339.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class A1339 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        long n;
+        while(t--!=0){
+            n = input.nextLong();
+            System.out.println(n);
+        }
+        input.close();
+    }
+}
