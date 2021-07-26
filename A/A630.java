1aafaa978c7ad76be7fa8acb398568405fa20be0
Darknez07 <rishikakkar42@gmail.com>
2021-07-27 00:17:13 +0530

    Level A series 600

---
+++ A/A630.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class A630 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long n = input.nextLong();
+        System.out.println(25);
+        input.close();
+    }
+}
