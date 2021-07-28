280151ebc014b2e4b18320e6287f2b8dd8202814
Darknez07 <rishikakkar42@gmail.com>
2021-07-29 00:22:09 +0530

    Level B series 1500

--- B/B1537.java
+++ B/B1537.java
@@ -1,25 +1,18 @@
 import java.util.Scanner;
-// Not complete
 public class B1537 {
-    /* Here we go /* Kaise */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int t = input.nextInt();
         long n,m,i,j;
         while(t--!=0){
-            m = input.nextLong();
             n = input.nextLong();
+            m = input.nextLong();
             i = input.nextLong();
             j = input.nextLong();
-            long val = Math.max(Math.max(Math.max((Math.abs(m - i) + Math.abs(n - j)),(Math.abs(m - i) + Math.abs(1 - j))),(Math.abs(n - j) + Math.abs(1 - i))),(Math.abs(1 - i) + Math.abs(1 - j)));
-            if(val == (Math.abs(m - i) + Math.abs(n - j))){
-                System.out.println(m+" "+n+" "+m+" "+1);
-            }else if(val == (Math.abs(m - i) + Math.abs(1 - j))){
-                System.out.println(m+" "+1+" "+(m-1)+" "+2);
-            }else if(val == (Math.abs(n - j) + Math.abs(1 - i))){
-                System.out.println(1+" "+n+" "+1+" "+(n-1));
+            if((i == 1 && j == 1) || (i == n && j == m)){
+                System.out.println(1+" "+m+" "+n+" "+1);
             }else{
-                System.out.println(1+" "+1+" "+1+" "+2);
+                System.out.println(1+" "+1+" "+n+" "+m);
             }
         }
         input.close();
