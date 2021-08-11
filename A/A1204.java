980730b57fa1dc279360f7eb6262163f882a7ae8
Darknez07 <rishikakkar42@gmail.com>
2021-08-12 02:02:13 +0530

    Level A series 1204:

--- A/A1204.java
+++ A/A1204.java
@@ -1,26 +1,18 @@
-import java.math.BigInteger;
 import java.util.Scanner;
-// Not complete failing
+
 public class A1204 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        BigInteger big = new BigInteger(input.next(), 2);
-        if(big.signum() == 0){
-            System.out.println(0);
-        }else{
-            double a= Math.log10(4.0);
-            double x = Math.log10(big.doubleValue())/a;
-            if(big.bitLength() > 80){
-                boolean res = big.testBit(0);
-                // boolean res2 = big.testBit(4);
-                if(res)
-                    System.out.println((int)(Math.ceil(x) + 1));
-                else
-                    System.out.println((int)(Math.ceil(x)));
-            }else{
-                System.out.println((int)Math.ceil(x));
-            }
-        }
+        String s = input.next();
         input.close();
+        int n = s.length();
+        int res = (n + 1) / 2;
+        int i;
+        for(i=1;i<n;i++){
+            if(s.charAt(i) == '1')
+                break;
+        }
+        if(n % 2 == 1 && i == n) res--;
+        System.out.println(res);
     }
 }
