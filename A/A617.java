09a24488d6690e04d887835d8f2bac9114885bea
Darknez07 <rishikakkar42@gmail.com>
2021-08-14 00:53:10 +0530

    Level A series 600

--- A/A617.java
+++ A/A617.java
@@ -4,13 +4,9 @@ public class A617 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int dist = input.nextInt();
-        if(dist > 5)
-            if(dist % 5 == 0)
-                System.out.println(dist/5);
-            else
-                System.out.println(dist/5 + 1);
-        else
-            System.out.println(1);
+        int left = dist % 5;
+        dist/=5;
+        System.out.println(dist + (left !=0 ? 1: 0));
         input.close();
     }
 }
