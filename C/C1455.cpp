d5bd2fb171962c81fc8ad2c1a2417fc369028a08
Darknez07 <rishikakkar42@gmail.com>
2021-10-11 00:12:56 +0530

    Level C series 1400

---
+++ C/C1455.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t,x,y;
+    cin>>t;
+    while(t--){
+        cin>>x>>y;
+        cout<<(x - 1)<<" "<<y<<endl;
+    }
+    return 0;
+}
