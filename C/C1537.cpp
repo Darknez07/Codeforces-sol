a28a755361279cce1166bf88b1d955c50f262520
Darknez07 <rishikakkar42@gmail.com>
2022-01-03 04:50:54 +0530

    Level C series 1500

--- C/C1537.cpp
+++ C/C1537.cpp
@@ -20,15 +20,22 @@ int main(){
         }
         mn  = LONG_LONG_MAX;
         for(int i=1;i<n;i++){
+            // Find index of minimal diferrence
             if(mn > abs(v[i] - v[i - 1])){
                 mn = abs(v[i] - v[i - 1]);
                 pos = i;
             }
         }
+        // First using pos to n as they are sorted in
+        // ascending order
         for(int i=pos;i<n;i++)
             cout<<v[i]<<" ";
+        // Then 0 to pos - 1 then sort that they are also
+        // sorted in ascending order
         for(int i=0;i<pos;i++)
             cout<<v[i]<<" ";
+        // So the difference between the first and
+        // last is minimum
         cout<<endl;
     }
     return 0;
