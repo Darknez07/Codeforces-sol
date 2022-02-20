648dd2ef4f10d63f550867057be291df606f6a7f
Darknez07 <rishikakkar42@gmail.com>
2022-02-20 23:33:57 +0530

    Level B series 1300

---
+++ B/B1366.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void solve(){
+    int n,x,m,l,r,lid;
+    cin>>n>>x>>m;
+    vector<pair<int,int> > v(m,{0,0});
+    for(int i=0;i<m;i++) cin>>v[i].first>>v[i].second;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
